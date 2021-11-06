6e459c575c7177a3e04673f614bea16caa83fd2c
guillesanbri <guillesanbri@hotmail.com>
2021-11-06 09:47:01 +0100

    fix return

--- util/misc.py
+++ util/misc.py
@@ -76,5 +76,4 @@ def visualize_attention(input, model, prediction, model_type):
 def get_random_string(length):
     # choose from all lowercase letter
     letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    print("Random string of length", length, "is:", result_str)
+    return ''.join(random.choice(letters) for _ in range(length))
